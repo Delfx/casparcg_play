e1caecdedefdc86688a72edf98cc519c9802162f
Lukas Motiejūnas <motiejunas.lukas@gmail.com>
2019-04-17 16:25:11 +0300

    added timeout function

--- play_test.js
+++ play_test.js
@@ -2,29 +2,21 @@ const {CasparCG} = require("casparcg-connection");
 
 let connection = new CasparCG();
 
-(async () => {
-    await connection.play(1, 1, "pirmas");
-})();
+
+connection.play(1, 1, "pirmas");
 
 
 const broadcasting = async () => {
+    // connection.play(1, 1, "first");
+    await new Promise((resolve) => setTimeout(() => {
+        resolve();
+    }, 300));
     const playinfo = await connection.info(1, 1);
-
     const fileInfo = await playinfo.response.data.stage.layer.layer_1.foreground.file;
-
     console.log(fileInfo);
-
-    if (!fileInfo) {
-        connection.play(1, 1, "pirmas");
-    }
 
     let timefirst = fileInfo.time[0];
     let timesecond = fileInfo.time[1];
-
-    if (timesecond === 0) {
-        connection.play(1, 1, "pirmas");
-    }
-
 
     // setTimeout
     // setInterval
@@ -32,25 +24,21 @@ const broadcasting = async () => {
     // clearTimeout
     // clearInterval
 
-
     const intervalId = setInterval(async () => {
-
         const lastnumber = timesecond - timefirst;
-
-        if (lastnumber < 0.3) {
+        if (lastnumber < 0.5) {
             clearTimeout(intervalId);
-
             console.log('-- ISGROJO');
-
-            connection.play(1, 1, "antras");
-
+            // connection.play(1, 1, "antras");
             return;
         }
 
         const playinfo2 = await connection.info(1, 1);
         timefirst = playinfo2.response.data.stage.layer.layer_1.foreground.file.time[0];
         console.log(playinfo2.response.data.stage.layer.layer_1.foreground.file.time[0], timesecond);
-    }, 100);
+    }, 300);
+
+
 
 };
 
